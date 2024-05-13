00038d632aaf56845d6442dfaf01f23f918353c1
lucanovelli.boolean <luca.novelli.lavoro@gmail.com>
2024-05-13 10:54:43 +0200

    add: added method of ticket price calculation

--- js/script-ticket.js
+++ js/script-ticket.js
@@ -17,5 +17,30 @@ form.addEventListener("submit", function (event) {
     if (isNaN(km) || isNaN(age))
         location.reload(); //RICARICO LA PAGINA SE I VALORI NON SONO CORRETTI*/
 
-    console.log("KM ---> " + typeof (km) + "/// AGE ---> " + typeof (age)); //STAMPA DI DEBUG
+    //console.log("KM ---> " + typeof (km) + "/// AGE ---> " + typeof (age)); //STAMPA DI DEBUG
+});
+
+
+//METODO CALCOLO COSTO TICKET
+form.addEventListener("submit", function (event) {
+
+    event.preventDefault(); //MODIFICO IL COMPORTAMENTO DEL FORM
+
+    //DEFINIZIONE VARIABILI INPUT
+    const km = parseFloat(document.getElementById("kilometers").value);
+    const age = parseInt(document.getElementById("age").value);
+
+
+    let price_ticket = document.getElementById("price_ticket");
+
+    if (age < 18)
+        price_ticket.innerHTML = "Il prezzo del ticket è: " + ((km * 0.21) * 20) / 100 + " €";
+
+    else if (age > 65)
+        price_ticket.innerHTML = "Il prezzo del ticket è: " + ((km * 0.21) * 40) / 100 + " €";
+
+    else
+        price_ticket.innerHTML = "Il prezzo del ticket è: " + km * 0.21 + " €";
+
+    //console.log("KM ---> " + typeof (km) + "/// AGE ---> " + typeof (age)); //STAMPA DI DEBUG
 });
